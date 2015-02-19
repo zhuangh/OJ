5b1027874806f7f046af73eace80a76e44fe031c
zhuang <zhuanghoward@gmail.com>
2015-02-19 11:33:01 -0800

    finish the populating problem verison II

--- leetcode/cpp/populating-next-right-pointers-in-each-node-ii.cpp
+++ leetcode/cpp/populating-next-right-pointers-in-each-node-ii.cpp
@@ -15,49 +15,87 @@ struct TreeLinkNode {
 
 class Solution {
 public:
-    void connect(TreeLinkNode *root) {
+    void connect( TreeLinkNode * root){
 
 	if( root == NULL) return;
-	if( root->left == NULL){ root-next = NULL; return;}
+
+	if( root->left && root->right){
+	    root->left->next = root->right;
+	}
+
+	if( root->next){
+
+	    TreeLinkNode * left = root->right? root->right: root->left;
+	    TreeLinkNode * right = NULL;
+	    for( TreeLinkNode * it = root->next; it !=NULL; it=it->next){
+		if( it->left ) {right = it->left; break;}
+		if( it->right) {right = it->right; break;}
+	    }
+	    if( left ) left->next = right;
+	}
+
+
+	connect(root->right);
+	connect(root->left);
+    
+    }
+
+
+
+    void connect1(TreeLinkNode *root) {
+
+	if( root == NULL) return;
+	if( root->left == NULL){ root->next = NULL; return;}
 
 	root->next = NULL;
 	TreeLinkNode * next_line = NULL;
 
 	if(root->right){
 	    root->right->next = NULL;
-	    next_line = root->right;
-	}
-	if(root->left) {
-	    root->left->next = root->right;
-	    next_line = root->left;
+	    if( root->right->left || root->right->right) next_line = root->right;
 	}
 
+	if(root->left) {	    
+	    root->left->next = root->right;	  
+	    if( root->left->left || root->left->right) next_line = root->left;	
+	}
+	cout<<next_line<<endl;
 
-	while(next_line->left != NULL){
-	    //	    cout<<next_line<<" "<<endl;
+	while(next_line != NULL){
 	    TreeLinkNode * cur = next_line;
-
-	    // cout<<next_line->val<<" "<<endl;
-	    TreeLinkNode * child = cur->left;
-	    TreeLinkNode * targo = cur->left;
+	 // cout<<next_line->val<<" "<<endl;
+	    TreeLinkNode * prev = NULL ;//= cur->left;
+	    int flag = 0;
 
 	    while( cur != NULL ){
+	//	cout<< cur->val<<" "<<endl;
 
-		if( child != NULL){
-		    if(targo !=NULL) {
-			targo->next = child;
-		    } 
-		    targo = child;
+		if( cur->left ){
+		    if( prev ){
+			prev->next = cur->left;
+		    }
+		    if(flag == 0 &&  ( cur->left->left || cur->left->right ) ){flag=1; next_line = cur->left;}
+		    prev = cur->left;
+
 		}
 
-		if( cur->left == right){
-		    cur=cur->next;
-		    child = cur->left;
+	//	cout<<prev->val<<" "<<endl;
+
+		if( cur->right){
+		    if( prev ){
+			prev->next = cur->right;
+		    }
+		    if(flag == 0 && ( cur->right->left || cur->right->right ) ){flag=1; next_line = cur->right;}
+		    prev = cur->right;
 		}
-		else{
-		    child = cur->right;
-		}
+	//	cout<<prev->val<<" "<<endl;
+
+		cur = cur->next;
+	    
 	    }
+	    if( flag == 0 ) next_line = NULL;
+	    prev->next = NULL;
+
 	}
     }
 };
@@ -65,12 +103,15 @@ public:
 int main(){
     TreeLinkNode *root = new TreeLinkNode(1);
     root->left = new TreeLinkNode(2);
+    root->right = NULL;
+    /*
     root->right = new TreeLinkNode(3);
     root->left->left = new TreeLinkNode(4);
-    root->left->right = new TreeLinkNode(5);
+    root->left->right = NULL;//new TreeLinkNode(5);
     root->right->left = NULL;
     //    root->right->left = new TreeLinkNode(6);
     root->right->right = new TreeLinkNode(7);
+    */
     Solution sol;
     sol.connect(root);
     //  sol.print(root);
