0234c42fb73fadab1270fbffca905edf6673849b
zhuang <zhuanghoward@gmail.com>
2015-11-08 19:33:15 -0800

    3rd trial

--- leetcode/cpp/RegularExpressionMatching.cpp
+++ leetcode/cpp/RegularExpressionMatching.cpp
@@ -23,7 +23,7 @@ isMatch("ab", ".*") → true
 isMatch("aab", "c*a*b") → true
 */
 
-//#define DEBUG
+// #define DEBUG
 
 class Solution{
 public:
@@ -34,32 +34,21 @@ public:
 	sp[0][0] = 1;
 
 	// sp[i][j] = 1 means s[1:i] matches p[1:j] 
-	// sp[0][j] = 1 means empty s matches p[1:j]  
-
-	// pay attention to the initialization 
-//	for(int j = 1 ; j < pl; j++){
-//	    sp[0][j] = 1;
-//	}
-
-	/*
-	for(int i = 0 ; i < sl; i++){
-	    sp[i][0] = 1;
-	}
-	*/
-
 	for ( int i = 1 ; i < sl ; i++){
 	    for ( int j = 1 ; j < pl ; j++){
 		if( p[j-1] == s[i-1] && sp[i-1][j-1] == 1) sp[i][j] = 1;
 		else{
 		    if( p[j-1] == '.' && sp[i-1][j-1] == 1) sp[i][j] =1;
-		    if ( p[j-1] == '*' && (sp[i-1][j-1] == 1 || sp[i-1][j-2] == 1)) 
+		    if ( p[j-1] == '*' )
 		    {
+			if (sp[i-1][j-1] == 1)  sp[i][j] = 1; // correct character in p[j-2]*, so extend  
+			if (sp[i-1][j-2] == 1) sp[i-1][j] = 1; // wrong character in p[j-2]*  
+
 #ifdef DEBUG
 			cout<<i<<" , "<<j<<":" ;
 			cout<<p[j-2]<<endl;
 			cout<<sp[i-1][j-1] << " , " <<sp[i-1][j-2]<<endl;
 #endif
-			sp[i][j] =1;
 		    }
 		}
 	    }
@@ -80,7 +69,6 @@ int main(){
     Solution a ; 
 
     cout<<   a.isMatch("aaa","aaaa")   <<endl; 
-
     cout<<   a.isMatch("aa","a")   <<endl; 
     cout<<   a.isMatch("aa","aa")  <<endl;  
     cout<<   a.isMatch("aaa","aa") <<endl;
@@ -88,6 +76,7 @@ int main(){
     cout<<   a.isMatch("aa", ".*") <<endl;
     cout<<   a.isMatch("ab", ".*") <<endl;
     cout<<   a.isMatch("aab", "c*a*b") <<endl ;
+    cout<<   a.isMatch("aaa", "ab*a") <<endl ;
     return 0; 
 }
 
