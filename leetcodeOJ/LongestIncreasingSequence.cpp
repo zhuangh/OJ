b3818f086515cb9bcabe164d2edf0990bf8c54ea
zhuang <zhuanghoward@gmail.com>
2015-11-18 14:41:26 -0800

    be careful with the default lower_bound's comparison function for vector<pair< int, int> >

--- leetcodeOJ/LongestIncreasingSequence.cpp
+++ leetcodeOJ/LongestIncreasingSequence.cpp
@@ -1,7 +1,31 @@
 #include<stdio.h>
 #include<iostream>
 #include<vector>
+#include<algorithm>
+#include<utility>
+
 using namespace std;
+
+class IdxNum{
+private:
+    int value;
+    int index;
+
+public:
+    IdxNum(int a , int b): value(a), index(b){};
+
+    bool operator < ( const  IdxNum & a) const {
+	return value < a.value;
+    }
+
+    int getValue() const {
+	return value;
+    }
+
+    int getIndex() const {
+	return index;
+    }
+};
 
 class Solution {
 public:
@@ -12,7 +36,6 @@ public:
 	    vector<int> seq( nums.size(), 1);
 	    seq[0] = 1;
 	    int max_seq = 1;
-
 
 	    for( int i = 0 ; i < nums.size() ; i++){
 		for( int j = 0 ; j < i ; j++){
@@ -28,9 +51,52 @@ public:
 	// nlogn
 	// also check the acm/icpc template by Jaehyun Park (poj: router) from Stanford
 	// http://stanford.edu/~liszt90/acm/notebook.html#file28 
-        int lengthOfLIS(vector<int>& nums) {
+	/*
+	   bool cmp ( pair<int, int> &a, pair<int, int> &b) {
+	   return a.first < b.first; 
+	   }
+	 */
 
+	int lengthOfLIS(vector<int>& nums) {
+//	    cout<<endl;
+
+	    if(nums.size() == 0 || nums.size() == 1  ) return nums.size();
+	    vector< pair<int, int> > LIS_seq ; 
+	    for( int i = 0 ;  i < nums.size(); i++){
+//		cout<<nums[i] <<endl;
+//		print(LIS_seq);
+#ifdef STRICT_INCREASING
+		pair<int, int> item = make_pair ( nums[i], 0 ) ; // watch the 0 here, 
+		// assumption < 
+		//    it.first < item.first
+		//    it.second < it.second 
+		vector<pair<int, int> >::iterator it = lower_bound( LIS_seq.begin(), LIS_seq.end(), item  ) ; 
+#else
+		pair<int, int> item = make_pair ( nums[i], i ) ;
+		vector<pair<int, int> >::iterator it = upper_bound( LIS_seq.begin(), LIS_seq.end(), item  ) ; 
+#endif
+		if(  it == LIS_seq.end()) { 
+		    LIS_seq.push_back( item ); 
+		}
+		else{
+		    *it = item;
+		}
+	    }
+
+	    return LIS_seq.size()  ;
 	}
+
+	void print( vector< pair<int, int> > tmp ){
+	    for(int i = 0 ; i < tmp.size(); i++){
+		cout<< tmp[i].first <<" " ;
+	    }
+	    cout<<endl;
+	    for(int i = 0 ; i < tmp.size(); i++){
+		cout<< tmp[i].second <<" " ;
+	    }
+	    cout<<endl;
+	}
+
 
 };
 
@@ -38,8 +104,15 @@ int main(){
 
     Solution a; 
     vector<int> num; 
-//    num = {10, 9, 2, 5, 3, 7, 101, 18, 19};
-    num = {1,3,6,7,9,4,10,5,6};
-    cout<<a.lengthOfLIS( num)<<endl;
+//    num = {3,5,6,2,5,4,19,5,6,7,12};
+    num= {2,2, 2};
+//    num = {1, 10, 9, 2, 5, 3, 7, 101, 18, 19};
+
+//    num = {1,3,6,7,9,4,10,5,6};
+    for(int i = 0 ; i < num.size(); i++){
+	cout<< num[i]<<" " ;
+    }
+    cout<<endl;
+    cout<<a.lengthOfLIS( num) <<endl;
     return 0;
 }
