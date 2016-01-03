5faafa3b50656d7b05d1071397c690dfa7017acb
zhuang <zhuanghoward@gmail.com>
2016-01-03 12:36:55 -0800

    fixed course schedule TLE

--- ltOJ/courseSchedule.cpp
+++ ltOJ/courseSchedule.cpp
@@ -6,40 +6,46 @@ using namespace std;
 
 class Solution {
 public:
-    void DFS( vector< vector<int> > & adj,  map<int, bool> & visited, int where, int from, bool & cycle){
+    void DFS( vector< vector<int> > & adj,  vector<bool> & visited, vector<bool> & path, int where, int from, bool & cycle){
 	for( auto it : adj[where]) {  
-	    if( it == from ) {
+	    if( path[it] == true ) {
 		cycle = true;
 		return ;
 	    }
+	    if( cycle == true) return; 
 	    if( visited[it] == false)   {
 		visited[it] = true;
+		path[it] = true;
 		// cout<<"Processing " << it <<endl;
-		DFS(adj, visited, it , from, cycle);
-		visited[it] = false;
+		DFS(adj, visited, path,  it , from, cycle);
+		// visited[it] = false; // <---- cause TLE 
+		path[it] = false;
 	    }
 	}
     }
 
 
     bool canFinish(int numCourses, vector<pair<int, int>>& prerequisites) {
-	map<int, bool> visited;
+	vector<bool> visited (numCourses, false);
+	vector<bool> path (numCourses, false);
 	vector< vector<int> > adj( numCourses) ;
 	bool cycle = false;
 	for( auto it : prerequisites){
 	   // cout<<it.first<<endl;
 	    adj[it.first].push_back(it.second);
 	    visited[ it.first ] = false; 
+	    path[ it.first ] = false; 
 	    visited[ it.second ] = false; 
+	    path[ it.second ] = false; 
 	}
 
 	for( int it =0 ; it < numCourses; it++){
 //	    vector<int> towhereset = adj[it];
 //	    for( auto towhere: towhereset){ 
 		// cout<<"Process " << it <<endl;
-		DFS( adj, visited, it , it , cycle); 
+	    if( visited[it] == false )  DFS( adj, visited, path, it , it , cycle); 
 		// cout<<" cycle deteced "<<cycle<<endl;
-		if(cycle == true) return false;
+	    if( cycle == true ) return false;
 	}
 	return true;
     }
@@ -47,7 +53,8 @@ public:
 
 int main(){
     Solution a ;
-    vector<pair< int , int > > prep = {{0,1},{1,2},{2,3}, {2,4},{3,4} };
+    // vector<pair< int , int > > prep = {{0,1},{1,2},{2,3}, {2,4},{3,0} };
+    vector<pair< int , int > > prep = {{1,0},{2,0}};
     cout<<a.canFinish( 5 , prep)<<endl;
     return 0;
 }
