448244a4b663bce156bcb93f518b42c748b15df6
zhuang <zhuanghoward@gmail.com>
2016-01-10 21:03:33 -0800

    hand coded heap

--- ltOJ/README.md
+++ ltOJ/README.md
@@ -1,8 +1,6 @@
 
 Problems pending: 
 
-Dijkstra's algorithm https://leetcode.com/problems/word-ladder/
-
 https://leetcode.com/problems/dungeon-game/
 
 https://leetcode.com/problems/add-and-search-word-data-structure-design/  Do it again for Convert Sorted List to Binary Search Tree using AVL . 
@@ -12,8 +10,6 @@ https://leetcode.com/problems/number-of-islands/ try to use union-find
 leetcode.com/problems/reverse-linked-list-ii/ 
 
 Try divide and conquer problems https://leetcode.com/problems/median-of-two-sorted-arrays/
-
-Try graph algorithm problems https://leetcode.com/problems/minimum-height-trees/
 
 Try trie problems   https://leetcode.com/problems/word-search-ii/
 
--- ltOJ/wordLadder.cpp
+++ ltOJ/wordLadder.cpp
@@ -2,12 +2,15 @@
 #include<unordered_set>
 #include<string>
 #include<vector>
+#include<queue> 
+#include<map> 
 
 using namespace std;
 
 class Solution {
 public:
-    int ladderLength(string beginWord, string endWord, unordered_set<string>& wordList) {
+    // solution 1 
+    int ladderLength1(string beginWord, string endWord, unordered_set<string>& wordList) {
 	vector<string> q_cur, q_next;
 	q_cur.push_back(beginWord);
 	int level = 0;
@@ -47,16 +50,120 @@ public:
 
 	return 0;
     }
+
+    // solution 2 using Dijkstra's algorithm
+
+
+    int ladderLength(string beginWord, string endWord, unordered_set<string>& wordList) {
+	typedef pair<int, int> PII;
+	int n = wordList.size();
+
+
+	//cout<<"word list size = "<<n+2<<endl;
+
+	map<string, int> index_mapping ; 
+	int cnt  = 0 ; 
+	index_mapping[beginWord] = cnt++;
+	index_mapping[endWord] = cnt++;
+
+
+	for( auto it : wordList ){
+	    index_mapping[it] = cnt++;
+	}
+
+	vector<vector<PII> > edges(cnt);  
+	// cout<<cnt<<endl;
+	map< PII, bool> visited_edge;
+
+
+	
+	wordList.insert( beginWord ); 
+	wordList.insert( endWord ); 
+
+	for( auto it : wordList ){
+	    for( int  i = 0; i<26;i++){
+		int a = index_mapping[it];
+		char ch = i + 'a'; 
+		for( int j = 0 ; j < it.length(); j++){
+		    string target = it;   
+		    target[j] = ch;
+		    visited_edge[ make_pair(a,a)] = true;
+		    if(
+		       wordList.find( target ) != wordList.end() 
+		      ){ 
+			int b = index_mapping[target];
+			visited_edge[ make_pair(b,b)] = true;
+			if ( visited_edge[make_pair(a,b)] == false){ 
+			    edges[a].push_back( make_pair(b, 1));
+			    edges[b].push_back( make_pair(a, 1) );
+			    visited_edge[ make_pair(a,b) ] = true;
+			    visited_edge[ make_pair(b,a) ] = true;
+			}
+		    }
+		}
+	    }
+	}
+
+
+
+	/*
+
+	int iter = 0 ;
+	for(auto it : edges){
+	    cout<<iter<<" --- "<<" : ";
+	    for(auto jt : it){
+		cout<<jt.first<<" distance "<<jt.second <<"; ";
+	    }
+	    iter++;
+	    cout<<endl;
+	}
+	*/
+
+
+	// after graph constrauction 
+
+	priority_queue<PII, vector<PII>, greater<PII> > Q; 
+	int INF = 100000000;
+	vector<int> dist (cnt, INF) ;
+	vector<int> dad (cnt, -1);
+	dist[0] = 0;
+
+	Q.push ( make_pair ( 0 , 1 ));  
+	while( ! Q.empty() ){
+	    PII p = Q.top();
+	    if ( p.first == 1 )  break;
+	    Q.pop();
+	    int here = p.first; 
+	    for( auto it : edges[here] ){
+		if( dist[it.first] > dist[here] + it.second){
+		dist[ it.first ] = dist[here] + it.second;
+		dad[ it.first ]= here;
+		Q.push(make_pair ( it.first, it.second ));
+		}
+	    }
+	}
+
+	/*
+	for( int i = 0 ; i < cnt ;i++){
+	    cout<<dist[i]<<endl;
+	}
+	*/
+
+	if( dist[1] < INF) { return 1; } 
+	return 0;
+    }
+
 };
 
 int main(){
-    unordered_set<string> wlist( {"hot", "dot" , "dog", "lot"} );
-//    unordered_set<string> wlist( {"hot",  "dog"} );
+//    unordered_set<string> wlist( {"hot", "dot" , "dog", "lot"} );
+    unordered_set<string> wlist( {} );
     // unordered_set<string> wlist;
     Solution a;
     
     //cout<<a.ladderLength("hot","dog",wlist)<<endl;
-    cout<<a.ladderLength("hit","cog",wlist)<<endl;
+    // cout<<a.ladderLength("hit","cog",wlist)<<endl;
+    cout<<a.ladderLength("cot","hot",wlist)<<endl;
     return 0;
 }
 
