c584b8da1478e6fb3876e9f90e39a015c1d32519
zhuang <zhuanghoward@gmail.com>
2016-02-01 13:44:24 -0800

    radix sorting flow repick

--- ltOJ/merge-k-sorted-lists.cpp
+++ ltOJ/merge-k-sorted-lists.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include<priority_queue>
+#include<queue>
 #include<vector>
 using namespace std;
 
@@ -10,6 +10,7 @@ struct ListNode {
 };
 
 
+class Solution{
 public:
     struct mycmp{
         bool operator() (const ListNode *   a, const  ListNode *  b) const{
@@ -50,10 +51,59 @@ public:
        return newlist_head.next;
     }
 
+    // counting sort and radix sort 
+    void counting( vector<int> & arr , int num_base ){
+	vector<int> count  (10,0);
+	vector<int> output (arr.size(),0);
+	
+	int n = arr.size();
+	for( int i = 0; i < n ; i++){
+	    count[ (arr[i]/num_base) %10]++;
+	}
+
+	for( int i = 1 ; i < 10 ; i++){
+	    count[i] += count[i-1];
+	}
+
+	for( int i = n - 1; i >=0 ;i--){
+	   output[ count[ ( arr[i]/num_base) %10] -1 ] = arr[i]; 
+	   count[ (arr[i]/num_base) % 10 ]--;
+	}
+	
+	for( int i = 0 ; i < n; i++){
+	    arr[i] = output[i];
+	}
+
+    } 
+
+    int getmax( vector<int> arr ) {
+	if( arr.size() <= 0 ) return -1; 
+	int mx = arr[0];
+	for( int i = 1 ; i < arr.size() ; i++){
+	    mx = arr[i]>mx? arr[i]:mx;
+	}
+	return mx;
+    }
+
+    vector<int> radixSort( vector<int> & arr ){
+	int m = getmax(arr); 
+//	cout<<m<<endl;
+	for( int num_base = 1 ; m / num_base > 0; num_base *=10){
+//	    cout<<num_base<<endl;
+	    counting( arr , num_base) ;
+	}
+	return arr;
+    }
 };
 
 int main(){
 
+    Solution a;
+    vector<int> nums {10,9,1,20,3,2};
+
+    for( auto it : a.radixSort( nums )  )
+	cout<<it<<endl;
 
     return 0; 
 }
+
