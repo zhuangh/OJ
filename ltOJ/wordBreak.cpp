faf23f4b9f9ac558c8ab6605bce36fdc40e8ccf6
zhuang <zhuanghoward@gmail.com>
2016-02-02 20:41:13 -0800

    a^b

--- ltOJ/wordBreak.cpp
+++ ltOJ/wordBreak.cpp
@@ -6,6 +6,8 @@
 #include<vector>
 #include<algorithm>
 
+#include<string>
+
 
 using namespace std;
 
@@ -294,6 +296,373 @@ public:
 	    }
 	}
     }
+    vector<char> gencand( char ss ){
+	if((ss - '9') == 0 ) return vector<char>{'w','x','y','z'};
+	if((ss - '8') == 0 ) return vector<char>{'t','u','v'};
+	if((ss - '7') == 0 )  return vector<char>{'p','q','r','s'};
+	int of =  (int) (ss-'2');
+	//	cout<<of<<endl;
+	return vector<char>{ (char) ('a' + 3*of) ,  (char) ('b'+ 3*of),  (char) ('c'+ 3*of)};
+
+    }
+    void gen( int st, int en,  string digits, vector<string> &ss ){
+	if( st > en) {
+	    return; 
+	}
+	int sz = ss.size();
+	vector<char> c_t = gencand( digits[st] );
+	for( int i = 0 ; i < sz  ; i++){
+	    for ( auto it : c_t ){
+		string tmp = ss[i];
+		tmp += it ;
+		//		cout<<tmp<<endl;
+		ss.push_back(tmp);
+	    }
+	}
+	gen(st+1, en, digits, ss);
+    }
+
+    vector<string> letterCombinations11(string digits) {
+	int sz = digits.length();
+	if (sz < 1 ) return {};
+	vector<string> ss ;
+	vector<char> c_t = gencand( digits[0] ); 
+	for( auto it : c_t ){
+	    string tmp = "";
+	    tmp+=it;
+	    //	    cout<<it<<endl;
+	    ss.push_back( tmp ); 
+	    //	    printf("%s\n",tmp.c_str() );
+	}            
+	gen(1, sz-1 , digits, ss);
+	vector<string> sss;	
+	for( auto it : ss ) {if(it.size() == digits.length() ) sss.push_back(it);}
+
+	return sss;
+    }
+
+
+
+    vector<string> letterCombinations(string digits) {
+	unordered_map<char, vector<char> > mm{
+	    {'2', {'a','b','c' }},
+		{'3', {'d','e','f' }},
+		{'4', {'g','h','i' }},
+		{'5', {'j','k','l' }},
+		{'6', {'m','n','o' }},
+		{'7', {'p','q','r','s' }},
+		{'8', {'t','u','v' }},
+		{'9', {'w','x','y','z' }}
+	}; 
+	vector<string> mms;
+	int n = digits.size()-1;
+	string path = "";
+	//auto it = mm['2'];
+	//	cout<<it[0]<<endl;
+	//	cout<<digits.size()<<endl;
+	dfs_gen( mm, mms, digits, 0 , n, path);
+	return mms;
+    }
+
+    void dfs_gen( unordered_map<char, vector<char> > mm, 
+		  vector<string> & mms, string digits, 
+		  int st, int en, string  path){
+	if(st>en) {
+	    mms.push_back( path );
+	    return;
+	}
+
+	char c = digits[st]; 
+	//	vector<char> ss = mms[ c ] ;
+	auto ss = mm[c];
+	for( int i = 0 ; i < ss.size() ; i++){
+	    string tmp_path = path+ ss[i]; 
+	    dfs_gen( mm , mms, digits, st+1, en, tmp_path);
+	} 
+	return ;
+    }
+
+    // Programming Perl page 77
+    int longestConSum( vector<int> nums){
+	vector<int> key(nums.size() , -1);
+	vector<int> sol(nums.size() , 0);
+	// sol (0 , n-1 ) = 
+	// max ( nums[n-1], 
+	//  sol(0,n-2) + nums[n-1] when key[n-2] == n-2 
+	//  sol(0,n-2) when key [n-2] != n-2
+
+	if( nums[0] >= 0 ) {
+	    key[0] =0;  
+	    sol[0] = nums[0];
+	}
+
+	int maxval = nums[0];
+	for( int i = 1 ; i < nums.size() ;i++){
+
+	    if( key[i-1] == i-1) {
+		sol[i] = max( nums[i] + sol[i-1], nums[i]);
+		key[i] = i;
+	    }
+	    else{
+		sol[i] = max( nums[i] , sol[i-1]);
+		key[i] = nums[i] > sol[i-1] ? i : key[i-1]; 
+	    }
+	    maxval = maxval < sol[i] ? sol[i] : maxval;
+	}
+	for( auto it : sol)
+	    cout<<it<<" ";
+	cout<<endl;
+
+	return maxval;//sol[nums.size()-1];
+    }
+
+    int longestConsecutiveUnionFind(vector<int>& nums) {
+	if( nums.size() < 2) return nums.size(); 
+
+	vector<int> id (nums.size(), -1);
+	vector<int> ss (nums.size(), 1);
+
+	unordered_map<int,int> record; 
+
+	for(int i = 0 ; i < nums.size(); i++){
+	    id[i] = i ; 
+	}
+
+	for( int i = 0 ; i < nums.size() ;i++){
+	    if ( record.find(nums[i]) != record.end() ) continue;
+
+	    record[ nums[i] ] = i ;
+	    if ( record.find(nums[i]-1) != record.end() ) union_find( i, record[ nums[i] -1 ] , ss , id);
+	    if ( record.find(nums[i]+1) != record.end() ) union_find( i, record[ nums[i] +1 ] , ss , id);
+	}
+
+	int maxval = 1;        
+	for( auto it : ss ) maxval = maxval < it ? it : maxval;
+	return maxval;
+    }
+
+    int find_root( int idx , vector<int> & id ){
+	while( idx != id[idx]){
+	    idx = id[idx]; 
+	}
+	return idx;
+    }
+
+    void union_find( int id1, int id2 , vector<int> & ss, vector<int> & id){
+	int i = find_root(id1,id); 
+	int j = find_root(id2,id);
+	if(i==j) return;
+	if( ss[i] > ss[j]) {
+	    id[j] = i ;
+	    ss[i] += ss[j];
+	}else{
+	    id[i] = j;
+	    ss[j] += ss[i];
+	}
+    }
+    int getmax( vector<int> nums)    {
+	int mx = nums[0];
+	for( int i = 1 ; i < nums.size(); i++ )
+	    mx = nums[i]>mx? nums[i]: mx;
+	return mx;
+    }
+
+    void count_sort( vector<int> & nums, int num_base){
+
+	vector<int> count (10, 0);
+	vector<int> output( nums.size(), 0);
+	int n = nums.size() ; 
+	for(int i = 0 ; i < n ; i++ ) {
+	    count[ (nums[i]/num_base) %10  ]++;
+	}
+
+	for( int i = 1 ; i < 10 ; i++){
+	    count[i] += count[i-1];    
+	}
+
+	for( int i = n-1; i >= 0 ; i--){
+	    output[ count[ (nums[i] / num_base) %10 ] -1 ] = nums[i];
+	    count[ (nums[i]/num_base)%10 ]--;
+	}
+
+	for( int i = 0 ; i < n ; i++)
+	    nums[i] = output[i];
+
+    }
+
+    void pass( vector<int> & nums, vector<int> & pos, vector<int> & neg ){
+	for(auto it : nums){
+	    if(it>=0 ) pos.push_back(it);
+	    else neg.push_back(-it);
+	}
+    }
+
+    void union_pass( vector<int> & nums, vector<int> & pos, vector<int> & neg ){
+	int cnt = 0 ; 	
+	if( neg.size() >0) {
+	    reverse(neg.begin(), neg.end());
+	    for(auto it : neg){
+		nums[cnt] = -it;
+		cnt++;
+	    }
+	}
+	if( pos.size()>0) {
+	    for(auto it : pos){
+		nums[cnt] = it;
+		cnt++;
+	    }
+	}
+    }
+
+    void radix_sort( vector<int> & nums){
+	if(nums.size() > 0 ) {
+	    int mx = getmax(nums);
+	    for( int num_base = 1 ; mx/num_base > 0 ;  num_base*=10 )  count_sort( nums, num_base);
+	}
+	// return nums;
+    }
+
+    int longestConsecutiveRadix(vector<int>& nums) {
+	if( nums.size() == 0 ) return 0;
+	if( nums.size() == 1 ) return 1;
+	int cnt = 1; 
+	vector<int> pos ; 
+	vector<int> neg ; 
+	pass(nums, pos,neg);
+
+	cout<<pos.size()<<endl;
+	cout<<neg.size()<<endl;
+	radix_sort(pos) ;
+	radix_sort(neg) ; 
+	union_pass(nums, pos, neg); 
+	//      vector<int> cnt;
+	int lst = 1; 
+	for( int i = 1 ; i < nums.size() ; i++){
+	    if( nums[i-1]+1 == nums[i])//  || nums[i-1] == nums[i]) 
+	    {   
+		cnt++;
+	    }
+	    else if (nums[i-1] == nums[i]) {
+		cnt = cnt; // coverity trick?
+	    }
+	    else{
+		cnt=1;
+	    }
+	    lst = cnt>lst ? cnt:lst;
+	}
+	return lst; 
+    } 
+
+
+
+    int power( int a, int b){
+
+	int result = 1;
+
+	while( b  ){
+	    cout<<a<<endl;
+	    if( b & 1 ) { 
+		cout<<b<<" b & 1 "<< (b&1) <<endl;
+		result *= a;
+	    }
+	    b  >>= 1;
+	    a *= a;
+	}
+
+	cout<<result<<endl;
+	return result;
+
+	/*
+	int prod = a;
+	int i = 0;
+	int iter = (int) ( log(b) / log(2));
+	int lf = b% (1<<iter) ;
+	cout<<iter<<endl;
+	cout<<lf<<endl;
+	while( i < iter ){
+	    prod = prod * prod;
+	    i++;
+	}
+	for( int i = 0 ; i < lf ;i++) prod *= a;
+	return prod;
+	*/
+    }
+
+    void product( string & res, string p1, string p2){
+	
+	
+    }
+
+    string power_via_string( int a, int b){
+	string prod ;
+	int i = 0;
+	int iter = (int) ( log(b) / log(2));
+	int lf = b% (1<<iter) ;
+	cout<<iter<<endl;
+	cout<<lf<<endl;
+	while( i < iter ){
+	    product(prod, prod, prod);
+	    i++;
+	}
+	for( int i = 0 ; i < lf ;i++) product( prod, prod, std::to_string(a) );
+	return prod;
+    }
+
+
+};
+
+
+int main(){
+    Solution a;
+    unordered_set<string> wd ;
+    wd.insert("leet");
+    wd.insert("code");
+    wd.insert("cat");
+    wd.insert("cats");
+    wd.insert("sand");
+    wd.insert("and");
+    wd.insert("dog");
+    wd.insert("aaaa");
+    wd.insert("aa");
+    wd.insert("a");
+
+    //   wd.insert("leetcode");
+    //    cout<<a.wordBreakI("catsanddog", wd )<<endl;
+    //     for( auto it : a.wordBreak("catsanddog", wd))
+    //	cout<<it<<endl;
+
+    //    for( auto it : a.wordBreak("aaaaaaa", wd))
+
+    //    a.gameOfLife(board);
+    /*
+       vector<int> nums = {1,2,0,1};
+       for(auto it : a.subsets(nums))
+       {
+       for(auto itt : it)
+       cout<<itt<<" ";
+       cout<<endl;
+       }
+     */
+    vector<int> nums = {100,4,1,2,3,0,200};
+    cout<<endl<<" lcs "<< a.longestConsecutiveRadix(nums)<<endl;
+    cout<<a.power(2,10)<<endl;
+//    cout<<a.power_via_string(2,9)<<endl;
+    /*
+       string ss = " ";//"hello kitty";// " ; 
+       a.reverseWords(ss );
+       cout<<ss<<endl;
+
+
+       for( auto it : a.letterCombinations("23"))
+       cout<<it<<endl;
+
+       vector<int> nums1 = {1,2,0,1, -2 ,1};
+       cout<<a.longestConSum(nums1)<<endl;
+     */
+    return 0;
+
+}
+
     /*
        vector<vector<int>> threeSumLTE(vector<int>& nums) {
 
@@ -334,50 +703,6 @@ res_gl.push_back(res[i]);
 return res_gl;
 }
      */
-vector<vector<int>> threeSum(vector<int>& nums) {
-
-    vector<vector<int> > res;
-    sort(nums.begin(), nums.end());
-    int sz = nums.size();
-
-    if( sz<=2  ) return res;
-
-    int prev;
-
-    for( int i = 0; i < sz - 2 ; i++){
-	if( i>0) {
-	    while( prev == nums[i] ){ 
-		i++;
-		//  cout<<i<<endl;
-		if (i >= sz - 2 ) goto L1;
-	    }
-	}
-
-	int sum = -nums[i];
-	int j = i + 1; 
-	int k = sz - 1; 
-	while ( j < k  ){
-	    int trip2 = nums[j];
-	    int trip3 = nums[k];
-	    if( nums[j] + nums[k] == sum){
-		res.push_back(vector<int> {nums[i],nums[j],nums[k]});
-		while( nums[k] == trip3 && k>j) k--;
-		while( nums[j] == trip2 && k>j) j++;
-	    }
-	    else if( nums[j] + nums[k] > sum) { 
-		while( nums[k] == trip3 && k>j) k--;
-	    }
-	    else {
-		while( nums[j] == trip2 && k>j) j++;
-	    }
-
-
-	}
-	prev= nums[i];
-    }
-L1:
-    return res;
-}
 /*
    vector<int> twoSum(vector<int>& nums, int target) {
 // sort(nums.begin(),nums.end());
@@ -410,311 +735,3 @@ if(idd > 0 && idd!=i+1) return vector<int> {i+1,idd};
 }
 }
  */
-vector<char> gencand( char ss ){
-    if((ss - '9') == 0 ) return vector<char>{'w','x','y','z'};
-    if((ss - '8') == 0 ) return vector<char>{'t','u','v'};
-    if((ss - '7') == 0 )  return vector<char>{'p','q','r','s'};
-    int of =  (int) (ss-'2');
-    //	cout<<of<<endl;
-    return vector<char>{ (char) ('a' + 3*of) ,  (char) ('b'+ 3*of),  (char) ('c'+ 3*of)};
-
-}
-void gen( int st, int en,  string digits, vector<string> &ss ){
-    if( st > en) {
-	return; 
-    }
-    int sz = ss.size();
-    vector<char> c_t = gencand( digits[st] );
-    for( int i = 0 ; i < sz  ; i++){
-	for ( auto it : c_t ){
-	    string tmp = ss[i];
-	    tmp += it ;
-	    //		cout<<tmp<<endl;
-	    ss.push_back(tmp);
-	}
-    }
-    gen(st+1, en, digits, ss);
-}
-
-vector<string> letterCombinations11(string digits) {
-    int sz = digits.length();
-    if (sz < 1 ) return {};
-    vector<string> ss ;
-    vector<char> c_t = gencand( digits[0] ); 
-    for( auto it : c_t ){
-	string tmp = "";
-	tmp+=it;
-	//	    cout<<it<<endl;
-	ss.push_back( tmp ); 
-	//	    printf("%s\n",tmp.c_str() );
-    }            
-    gen(1, sz-1 , digits, ss);
-    vector<string> sss;	
-    for( auto it : ss ) {if(it.size() == digits.length() ) sss.push_back(it);}
-
-    return sss;
-}
-
-
-
-vector<string> letterCombinations(string digits) {
-    unordered_map<char, vector<char> > mm{
-	{'2', {'a','b','c' }},
-	    {'3', {'d','e','f' }},
-	    {'4', {'g','h','i' }},
-	    {'5', {'j','k','l' }},
-	    {'6', {'m','n','o' }},
-	    {'7', {'p','q','r','s' }},
-	    {'8', {'t','u','v' }},
-	    {'9', {'w','x','y','z' }}
-    }; 
-    vector<string> mms;
-    int n = digits.size()-1;
-    string path = "";
-    //auto it = mm['2'];
-    //	cout<<it[0]<<endl;
-    //	cout<<digits.size()<<endl;
-    dfs_gen( mm, mms, digits, 0 , n, path);
-    return mms;
-}
-
-void dfs_gen( unordered_map<char, vector<char> > mm, 
-	      vector<string> & mms, string digits, 
-	      int st, int en, string  path){
-    if(st>en) {
-	mms.push_back( path );
-	return;
-    }
-
-    char c = digits[st]; 
-    //	vector<char> ss = mms[ c ] ;
-    auto ss = mm[c];
-    for( int i = 0 ; i < ss.size() ; i++){
-	string tmp_path = path+ ss[i]; 
-	dfs_gen( mm , mms, digits, st+1, en, tmp_path);
-    } 
-    return ;
-}
-
-// Programming Perl page 77
-int longestConSum( vector<int> nums){
-    vector<int> key(nums.size() , -1);
-    vector<int> sol(nums.size() , 0);
-    // sol (0 , n-1 ) = 
-    // max ( nums[n-1], 
-    //  sol(0,n-2) + nums[n-1] when key[n-2] == n-2 
-    //  sol(0,n-2) when key [n-2] != n-2
-
-    if( nums[0] >= 0 ) {
-	key[0] =0;  
-	sol[0] = nums[0];
-    }
-
-    int maxval = nums[0];
-    for( int i = 1 ; i < nums.size() ;i++){
-
-	if( key[i-1] == i-1) {
-	    sol[i] = max( nums[i] + sol[i-1], nums[i]);
-	    key[i] = i;
-	}
-	else{
-	    sol[i] = max( nums[i] , sol[i-1]);
-	    key[i] = nums[i] > sol[i-1] ? i : key[i-1]; 
-	}
-	maxval = maxval < sol[i] ? sol[i] : maxval;
-    }
-    for( auto it : sol)
-	cout<<it<<" ";
-    cout<<endl;
-
-    return maxval;//sol[nums.size()-1];
-}
-
-int longestConsecutiveUnionFind(vector<int>& nums) {
-    if( nums.size() < 2) return nums.size(); 
-
-    vector<int> id (nums.size(), -1);
-    vector<int> ss (nums.size(), 1);
-
-    unordered_map<int,int> record; 
-
-    for(int i = 0 ; i < nums.size(); i++){
-	id[i] = i ; 
-    }
-
-    for( int i = 0 ; i < nums.size() ;i++){
-	if ( record.find(nums[i]) != record.end() ) continue;
-
-	record[ nums[i] ] = i ;
-	if ( record.find(nums[i]-1) != record.end() ) union_find( i, record[ nums[i] -1 ] , ss , id);
-	if ( record.find(nums[i]+1) != record.end() ) union_find( i, record[ nums[i] +1 ] , ss , id);
-    }
-
-    int maxval = 1;        
-    for( auto it : ss ) maxval = maxval < it ? it : maxval;
-    return maxval;
-}
-
-int find_root( int idx , vector<int> & id ){
-    while( idx != id[idx]){
-	idx = id[idx]; 
-    }
-    return idx;
-}
-
-void union_find( int id1, int id2 , vector<int> & ss, vector<int> & id){
-    int i = find_root(id1,id); 
-    int j = find_root(id2,id);
-    if(i==j) return;
-    if( ss[i] > ss[j]) {
-	id[j] = i ;
-	ss[i] += ss[j];
-    }else{
-	id[i] = j;
-	ss[j] += ss[i];
-    }
-}
-int getmax( vector<int> nums)    {
-    int mx = nums[0];
-    for( int i = 1 ; i < nums.size(); i++ )
-	mx = nums[i]>mx? nums[i]: mx;
-    return mx;
-}
-
-void count_sort( vector<int> & nums, int num_base){
-
-    vector<int> count (10, 0);
-    vector<int> output( nums.size(), 0);
-    int n = nums.size() ; 
-    for(int i = 0 ; i < n ; i++ ) {
-	count[ (nums[i]/num_base) %10  ]++;
-    }
-
-    for( int i = 1 ; i < 10 ; i++){
-	count[i] += count[i-1];    
-    }
-
-    for( int i = n-1; i >= 0 ; i--){
-	output[ count[ (nums[i] / num_base) %10 ] -1 ] = nums[i];
-	count[ (nums[i]/num_base)%10 ]--;
-    }
-
-    for( int i = 0 ; i < n ; i++)
-	nums[i] = output[i];
-
-}
-
-void pass( vector<int> & nums, vector<int> & pos, vector<int> & neg ){
-    for(auto it : nums){
-	if(it>=0 ) pos.push_back(it);
-	else neg.push_back(-it);
-    }
-}
-
-void union_pass( vector<int> & nums, vector<int> & pos, vector<int> & neg ){
-    int cnt = 0 ; 	
-    if( neg.size() >0) {
-	reverse(neg.begin(), neg.end());
-	for(auto it : neg){
-	    nums[cnt] = -it;
-	    cnt++;
-	}
-    }
-    if( pos.size()>0) {
-	for(auto it : pos){
-	    nums[cnt] = it;
-	    cnt++;
-	}
-    }
-}
-
-void radix_sort( vector<int> & nums){
-    if(nums.size() > 0 ) {
-	int mx = getmax(nums);
-	for( int num_base = 1 ; mx/num_base > 0 ;  num_base*=10 )  count_sort( nums, num_base);
-    }
-    // return nums;
-}
-
-int longestConsecutiveRadix(vector<int>& nums) {
-    if( nums.size() == 0 ) return 0;
-    if( nums.size() == 1 ) return 1;
-    int cnt = 1; 
-    vector<int> pos ; 
-    vector<int> neg ; 
-    pass(nums, pos,neg);
-
-    cout<<pos.size()<<endl;
-    cout<<neg.size()<<endl;
-    radix_sort(pos) ;
-    radix_sort(neg) ; 
-    union_pass(nums, pos, neg); 
-    //      vector<int> cnt;
-    int lst = 1; 
-    for( int i = 1 ; i < nums.size() ; i++){
-	if( nums[i-1]+1 == nums[i])//  || nums[i-1] == nums[i]) 
-	{   
-	    cnt++;
-	}
-	else if (nums[i-1] == nums[i]) {
-	    cnt = cnt; // coverity trick?
-	}
-	else{
-	    cnt=1;
-	}
-	lst = cnt>lst ? cnt:lst;
-    }
-    return lst; 
-} 
-};
-
-
-int main(){
-    Solution a;
-    unordered_set<string> wd ;
-    wd.insert("leet");
-    wd.insert("code");
-    wd.insert("cat");
-    wd.insert("cats");
-    wd.insert("sand");
-    wd.insert("and");
-    wd.insert("dog");
-    wd.insert("aaaa");
-    wd.insert("aa");
-    wd.insert("a");
-
-    //   wd.insert("leetcode");
-    //    cout<<a.wordBreakI("catsanddog", wd )<<endl;
-    //     for( auto it : a.wordBreak("catsanddog", wd))
-    //	cout<<it<<endl;
-
-    //    for( auto it : a.wordBreak("aaaaaaa", wd))
-
-    //    a.gameOfLife(board);
-    /*
-       vector<int> nums = {1,2,0,1};
-       for(auto it : a.subsets(nums))
-       {
-       for(auto itt : it)
-       cout<<itt<<" ";
-       cout<<endl;
-       }
-     */
-    vector<int> nums = {100,4,1,2,3,0,200};
-    cout<<endl<<" lcs "<< a.longestConsecutiveRadix(nums)<<endl;
-    /*
-       string ss = " ";//"hello kitty";// " ; 
-       a.reverseWords(ss );
-       cout<<ss<<endl;
-
-
-       for( auto it : a.letterCombinations("23"))
-       cout<<it<<endl;
-
-       vector<int> nums1 = {1,2,0,1, -2 ,1};
-       cout<<a.longestConSum(nums1)<<endl;
-     */
-    return 0;
-
-}
-
