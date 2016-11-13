79e56f2b04e9149417d46a49e28965e6a2498878
zhuang <zhuanghoward@gmail.com>
2016-11-13 15:12:23 -0800

    fix MLE for palindrome-pairs

--- ltOJ/palindromePairs336.py
+++ ltOJ/palindromePairs336.py
@@ -1,46 +1,59 @@
  
-
-class TrieNode: 
+#class TrieNode (object): 
+"""
     def __init__(self):
         self.leaves={}
         self.idx = [] 
         self.tag = -1
+"""
+
+class Solution(object):
 
     def is_pal ( self, s ):
         return s == s[::-1]
 
+    def is_pal_trie ( self, prefix, pal ):
+        if not self.leaves :
+            if self.is_pal( prefix , 0 , len(prefix)-1): 
+	        pal.append( idx )
+        else:
+            for c in cur.leaves:
+                self.is_pal_trie( prefix + c, pal  )
 
-    def insert(self, word, i) :
-        cur = self
+    def insert(self, word, i ,trie) :
+        cur = trie 
         cnt = 0 
         for c in word:
-            if not c in cur.leaves:
-                cur.leaves[c] = TrieNode()
-            cur = cur.leaves[c]
+            if not c in cur:
+                cur[c] = {} 
+            cur = cur[c]
             cnt += 1 
             tmp = word[cnt:]
             if tmp and self.is_pal(tmp):
-                 cur.idx.append(i) 
-#        cur.idx.append(i) 
-        cur.tag = i 
+                 cur.setdefault("idx",[]) 
+                 cur["idx"].append(i)  
+	
+        cur["tag"] = i
+        return trie
 
-    def find(self , word, i , res):
-        cur = self
+    def find(self , word, i , res, trie):
+        cur = trie 
         cnt = 0 
-        fail = False
+        fail = False 
 
         for c in word:
-            if not c in cur.leaves: 
+            if not c in cur: 
                 fail = True
                 break
-            cur = cur.leaves[c] 
+            cur = cur[c] 
             cnt += 1
-            tmp = cur.tag
-            if tmp != -1 and tmp!=i and self.is_pal( word[cnt:]):
-                 res.append([ tmp, i ]) 
+            if "tag" in cur:
+                tmp = cur["tag"]
+                if tmp != -1 and tmp!=i and self.is_pal( word[cnt:]):
+                    res.append([ tmp, i ]) 
 
-        if not fail and cur.idx :
-             for it in cur.idx:
+        if not fail and  "idx" in cur :
+             for it in cur["idx"]:
                  if it != i:
                      res.append([ it, i]) 
 
@@ -48,45 +61,29 @@ class TrieNode:
             #check pal for the left words of the target word
       
 
-class Solution(object):
+#class Solution(object):
     def palindromePairs(self, words):
         """
         : type words: List[str]
         :rtype: List[List[int]]
         """
-
-	res = []
-        trie = TrieNode()
+        res = []
+        trie ={}
+#        trie = TrieNode()
         for i in xrange( len(words) ) :
-            trie.insert(words[i] , i)  
-        for i in xrange( len(words) ):
-            print("searching "+words[i][::-1])
-            trie.find(words[i][::-1], i, res)
+            self.insert(words[i] , i, trie)  
+        for i in xrange( len(words) ): 
+            self.find(words[i][::-1], i, res, trie)
 
         if "" in words:  # check for "" case
             idx = words.index("")
             tidx = 0 
             for it in words:
                 if it != "":
-                   if trie.is_pal( it ):
-                       res.append([ idx, tidx ])
+                   if self.is_pal( it ):
+                       res.append([idx, tidx])
                        res.append([tidx, idx]) 
                 tidx += 1
     
         return res  
-
-if __name__ == '__main__' :
-
-    a = Solution()
-    words = ["abcd", "dcba", "lls", "s", "sssll"]
-    words = ["bat", "tab", "cat"]
-    words = ["a", ""]
-    words=["a","b","c","ab","ac","aa"]
-
-#words = ["abc","cba", "cat","tac"]
-#words = ["abbbbc", "abc", "abb", "ac"]
-
-    print(words)
-    print(a.palindromePairs(words))
-
-
+ 
