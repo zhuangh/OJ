7defd52912a0a24a8d690c179b679126b4d81767
zhuang <zhuanghoward@gmail.com>
2017-07-06 22:43:54 -0700

    python solution binary search of https://leetcode.com/problems/reverse-pairs/#/description

--- leetcode/longestPalindrome.cpp
+++ leetcode/longestPalindrome.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         std::vector< std::vector<int> > mm;
-        int tmp = 0 ;
-        int e = 0;
+        int tmp = 1;
+        int e = 1;
         int r = s.length();
         if(r<=1) return s;
         for(int i = 0 ; i < r+1 ;i++){
@@ -15,15 +15,17 @@ public:
             mm.push_back(m);
         }
         
-        for(int k = 2 ; k <= r; k++){
-            for(int i = 1; i < r - k ; i++ ){
+        for(int k = 2 ; k <= r; k++){ // k is the length of string to check
+            if(tmp > k) continue; // no need to compute the length small than the tmp 
+            for(int i = 1; i <= r - k + 1; i++ ){
                 int j = i + k -1;
                 if(s[i-1] == s[j-1]){
                     if(mm[i+1][j-1] != 0 || j - i <= 2) mm[i][j] = j - i + 1;
                     if(mm[i][j] > tmp) {tmp = mm[i][j]; e = i;}
                 }
             }
-        }
+        } 
         return s.substr(e-1, tmp);
     }
 };
+
