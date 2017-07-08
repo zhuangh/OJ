42918bc8d36361dfa0b4f835c5629678158dbf52
zhuang <zhuanghoward@gmail.com>
2017-07-08 11:31:19 -0700

    reorg leetcode

--- leetcode/rangeSumQuery.py
+++
@@ -1,9 +0,0 @@
-
-# interesting code for the weak test bench 
-# https://leetcode.com/discuss/70427/0-lines-python  
-class NumArray(object):
-    def __init__(self, nums) : 
-	self.update = nums.__setitem__ 
-	self.sumRange = lambda i, j : sum(nums[i:j+1]) 
-
-
