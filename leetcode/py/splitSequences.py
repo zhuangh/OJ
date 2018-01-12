c9661b901ff9e85149410b6384592ba3c6f2985e
zhuang <zhuanghoward@gmail.com>
2018-01-11 22:39:24 -0800

    python solution to https://leetcode.com/problems/split-array-into-consecutive-subsequences/description/

---
+++ leetcode/py/splitSequences.py
@@ -0,0 +1,26 @@
+class Solution:
+    def isPossible(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        tail_len_trace = {}
+        for it in nums: 
+            if it - 1 not in tail_len_trace:
+                if it not in tail_len_trace:
+                    tail_len_trace[it] = []
+                heapq.heappush(tail_len_trace[it], 1)
+            else:
+                l = heapq.heappop(tail_len_trace[it-1])
+                if it  not in tail_len_trace:
+                    tail_len_trace[it] = []
+                heapq.heappush(tail_len_trace[it], l+1)
+                if len(tail_len_trace[it-1]) == 0:
+                    del tail_len_trace[it-1]
+        #print(tail_len_trace)
+        for ending, arr in tail_len_trace.items():
+            for it in arr:
+                if it < 3: 
+                    return False
+        return True
+    
